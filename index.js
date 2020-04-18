6372a22c8b570b92290a8fa419632aa5c8e732ab
Fischerzera <designfischer@users.noreply.github.com>
2020-04-18 16:27:09 -0300

    mais uma tentativa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./src/routes');
 
 const app = express();
 
